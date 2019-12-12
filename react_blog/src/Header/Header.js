477d0ac809075f0fb2e90b9c3c32677695a146d4
Joseph Jin <jerkjoe61@gmail.com>
2019-12-12 14:01:03 -0800

    register

--- react_blog/src/Header/Header.js
+++ react_blog/src/Header/Header.js
@@ -8,17 +8,20 @@ import logo from '../logo.png';
 
 import './Header.css';
 import Login from '../Login/Login';
+import Register from '../Register/Register';
 
 export default function Header() {
     const { state, dispatch } = useContext(AppContext);
     
     let [loginAction, setLoginAction] = useState(false)
     
-    let [showModal, setShowModal] = useState(false)
+    let [showLoginModal, setShowLoginModal] = useState(false)
+    let [showRegisterModal, setShowRegisterModal] = useState(false)
+    
     
     useEffect(function() {
         console.log('123123123131231')
-        setShowModal(false)
+        setShowLoginModal(false)
     }, [
         state
     ])
@@ -34,14 +37,17 @@ export default function Header() {
             }
         })
     }
-    function openModal() {
-        setShowModal(true)
+    function openLoginModal() {
+        setShowLoginModal(true)
     }
-    function closeModal(event) {
-        setShowModal(false)
+    function openRegisterModal() {
+        setShowRegisterModal(true)
+    }
+    function closeLoginModal(event) {
+        setShowLoginModal(false)
     }
     
-    function onSubmit(event) {
+    function onLogin(event) {
         setLoginAction(!loginAction)
     }
     return (
@@ -53,11 +59,18 @@ export default function Header() {
                 {state.login ? (
                     <a onClick={handleLogout}>Logout</a>
                 ) : (
-                    <a onClick={openModal}>Login</a>
+                    <div>
+                        <a onClick={openLoginModal}>Login</a>
+                        <a style={{"padding-left": 20}} onClick={openRegisterModal}>Register</a>       
+                    </div>
+                                 
                 )}
             </div>
-            <Modal title="Login" visible={showModal} okText="Submit" onOk={onSubmit} onCancel={closeModal}>
+            <Modal title="Login" visible={showLoginModal} okText="Login" onOk={onLogin} onCancel={closeLoginModal}>
                 <Login login={loginAction}></Login>
+            </Modal>
+            <Modal title="Login" visible={showRegisterModal} okText="Login" onOk={onLogin} onCancel={closeLoginModal}>
+                <Register login={loginAction}></Register>
             </Modal>
         </header>
     );
--- react_blog/src/Register/Register.js
+++ react_blog/src/Register/Register.js
@@ -2,56 +2,64 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 
-export default function Register(){
-    
-    
-    
+export default function Register() {
+
+
+    const ERROR_MSG = 'Username and password cannot be empty.';
     let [showError, setShowError] = useState('error-display');
-    
-    
+    let [params, setParams] = useState({
+        username: '',
+        password: ''
+    });
+    let [submissionError, setSubmissionError] = useState('');
     function handleLogin() {
         return axios.post('http://nodeblog.josephjin.win/login', params);
     }
     function handleRegister() {
         return axios.post('http://nodeblog.josephjin.win/register', params);
     }
-    
+
     function handleClick(event) {
         event.preventDefault();
         console.dir(params);
         if (params.username.length && params.password.length) {
-            handleLogin()
-                .then(res => {
-                    console.log(res);
-                    let result = res.data;
-                    if (result.error) {
-                        console.log(result.message);
-                    } else {
-                        console.log(result.message);
-                        window.sessionStorage.setItem(
-                            'login_user',
-                            params.username
-                        );
-                        dispatch({
-                            type: 'UPDATE_LOGIN_STATUS',
-                            data: {
-                                login: true,
-                                login_user: params.username
-                            }
-                        });
-                    }
-                })
-                .catch(err => {
-                    console.log('err');
-                });
+            // handleLogin()
+            //     .then(res => {
+            //         console.log(res);
+            //         let result = res.data;
+            //         if (result.error) {
+            //             console.log(result.message);
+            //         } else {
+            //             console.log(result.message);
+            //             window.sessionStorage.setItem(
+            //                 'login_user',
+            //                 params.username
+            //             );
+            //             dispatch({
+            //                 type: 'UPDATE_LOGIN_STATUS',
+            //                 data: {
+            //                     login: true,
+            //                     login_user: params.username
+            //                 }
+            //             });
+            //         }
+            //     })
+            //     .catch(err => {
+            //         console.log('err');
+            //     });
+            handleRegister().then(res => {
+                console.log(res.data)
+            }).catch(err => {
+                console.log(err)
+            })
         } else {
             console.log('Error message');
             setShowError('error-display active');
         }
     }
-    
-    
-    
+
+
+
     function handleInputChange(event) {
         const name = event.target.name;
         const value = event.target.value;
@@ -68,18 +76,32 @@ export default function Register(){
     return (
         <div>
             <form name="login">
-                <input name="username" onChange={handleInputChange} />
-                <input
-                    name="password"
-                    type="password"
-                    onChange={handleInputChange}
-                    autoComplete="off"
-                />
-                <button onClick={handleClick}>Login</button>
-                <p className={showError}>{ERROR_MSG}</p>
+                <div className="inputs">
+                    <div>
+                        <label for="username">Username: </label>
+                        <input
+                            id="username"
+                            name="username"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label for="password">Password: </label>
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            onChange={handleInputChange}
+                            autoComplete="off"
+                        />
+                    </div>
+                </div>
+                <p className={showError}>
+                    {submissionError ? submissionError : ERROR_MSG}
+                </p>
             </form>
         </div>
     )
-    
-    
+
+
 }
